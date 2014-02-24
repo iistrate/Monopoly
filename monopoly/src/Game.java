f7791a723917a18355eca89cb938af8c7cda4291
Jay Bay <gratholio@gmail.com>
2014-02-24 18:19:26 -0500

    Working on player

--- monopoly/src/Game.java
+++ monopoly/src/Game.java
@@ -74,11 +74,11 @@ public class Game {
 			m_board.add(temptile.getPanel(), m_constraints);
 		}
 	}
-	// add tiles from tilelist in world.java to current frame
+	// add players from playerlist in world.java to current frame
 		void setupplayers(){
 			// temp Player object
 			Player tempplayer = null;
-			// temp tilelist 
+			// temp playerlist
 			ArrayList<Player> playerlist = new ArrayList<Player>();
 			playerlist = world.getPlayerlist();
 			for (int i = 0; i < playerlist.size(); i++){
