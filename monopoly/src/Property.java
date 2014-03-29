bea8c6ea22d019553d3c3aebd1517bbb1e89909d
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-03-29 19:14:23 -0400

    property builder pattern in place; built top row; constant ints for property types

--- monopoly/src/Property.java
+++ monopoly/src/Property.java
@@ -4,7 +4,32 @@ public class Property {
 	private boolean m_bavailable;
 	private int m_iplayer; 
 	
-	Property() {}
+	final int BROWN = 1;
+	final int LIGHT_BLUE = 2;
+	final int PURPLE = 3;
+	final int ORANGE = 4;
+	final int RED = 5;
+	final int YELLOW = 6;
+	final int GREEN = 7;
+	final int BLUE = 8;
+	final int UTILITIES = 9;
+	final int TRAIN_STATIONS = 10;
+	
+	
+	public Property() {}
+	
+	public Property Available(boolean b) {
+		m_bavailable = b;
+		return this;
+	}
+	public Property Value(int value) {
+		m_ivalue = value;
+		return this;
+	}
+	public Property Name(String name) {
+		m_sname = name;
+		return this;
+	}
 	
 	//setters
 	public void setAvailable(boolean b) {
--- monopoly/src/Tile.java
+++ monopoly/src/Tile.java
@@ -7,13 +7,50 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 public class Tile extends GameObject {
-	Tile(int w, int h, int x, int y, int sX, int sY) {
+	Property m_property;
+	Tile(int w, int h, int x, int y, int sX, int sY, boolean canBeBought) {
 		//initialize game objects members
 		m_w = w;
 		m_h = h;
 		m_x = x;
 		m_y = y;
 		m_panel = new JPanel();
+		if (canBeBought) {
+			if (y == 0) {
+				if (x == 1) {
+					m_property = new Property()
+					.Available(true).Name("Strand").Value(220);
+				}
+				else if (x == 3) {
+					m_property = new Property()
+					.Available(true).Name("Fleet Street").Value(220);
+				}
+				else if (x == 4) {
+					m_property = new Property()
+					.Available(true).Name("Trafalguar Square").Value(240);
+				}
+				else if (x == 5) {
+					m_property = new Property()
+					.Available(true).Name("Fenchurch Street Station").Value(200);
+				}
+				else if (x == 6) {
+					m_property = new Property()
+					.Available(true).Name("Leicester Square").Value(260);
+				}
+				else if (x == 7) {
+					m_property = new Property()
+					.Available(true).Name("Coventry Street").Value(260);
+				}
+				else if (x == 8) {
+					m_property = new Property()
+					.Available(true).Name("Water Works").Value(150);
+				}
+				else if (x == 9) {
+					m_property = new Property()
+					.Available(true).Name("Picadilly").Value(280);
+				}
+			}
+		}
 		//creates image
 		m_image = new CustomImage("assets/sprites-board.jpg", w, h, sX, sY);
 		//set panel size
